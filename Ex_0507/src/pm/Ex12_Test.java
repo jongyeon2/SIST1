4df72c647f0273052486d3d812ae7c46e1a31e0f
jongyeon <qorwhddus12@naver.com>
2025-05-08 08:58:03 +0900

    다시 업데이트

--- Ex_0507/src/pm/Ex12_Test.java
+++ Ex_0507/src/pm/Ex12_Test.java
@@ -28,12 +28,13 @@ public class Ex12_Test {
 		     * *
 		       *
 		*/
-		for(int k=1; k<=4; k++) {			
-			for(int i=1; i<=4; i++) {
-				if(k <= i) //k가 i보다 작으면 
-					System.out.printf("%2c",'*');
-				else 
+		for(int i = 0; i < 4; i++) {
+			for(int j = 0; j <4; j++) {
+				if(j >=i) {
+					System.out.printf("%2c", '*');
+				} else {
 					System.out.print("  ");
+				}
 			}
 			System.out.println();
 		}
