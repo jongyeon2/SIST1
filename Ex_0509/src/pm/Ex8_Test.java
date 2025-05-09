a08600c0d168d3d9916601fadca1907ee76a15b4
jongyeon <qorwhddus12@naver.com>
2025-05-09 18:49:41 +0900

    다시

--- Ex_0509/src/pm/Ex8_Test.java
+++ Ex_0509/src/pm/Ex8_Test.java
@@ -7,12 +7,23 @@ public class Ex8_Test {
 //		난수를 발생하여 배열 채우기 (1~45)
 		int[] lotto = new int[6];
 		
-		
-		
-		for(int i = 0; i < lotto.length; i++) {
-			lotto[i] = (int)(Math.random() * 45 + 1);
-				if(lotto[i] != lotto[i])
-		
+		for(int i = 0 ; i < lotto.length;) 
+		{
+			lotto[i] = (int)(Math.random() * 45 +1); //  lotto[1]에 난수 생성
+			for(int j = 0; j < i; j++)  //  j가 i보다 작을떄까지 반복 
+			{
+				if(lotto[i]==lotto[j]) // lotto0과 1이 같은거는  
+				{
+					i--;
+					break;
+				} 
+							
+			}
+			i++;
+			// ?????? i++
 		}
+		for (int i = 0; i<lotto.length;i++)
+			System.out.println(lotto[i]);
+		
 	}
 }
